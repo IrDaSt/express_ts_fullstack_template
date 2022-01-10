bf250fddba3f7d930afa91ae2245634f83001127
Daniel W <48021239+danielwj00@users.noreply.github.com>
2022-01-10 15:23:45 +0800

    mysql promise example

--- src/routes/api/books.ts
+++ src/routes/api/books.ts
@@ -1,7 +1,10 @@
+import upload from '@middlewares/multer'
 import { BooksModel } from '@models/books.model'
 import booksServices from '@services/api/books.services'
 import responses from '@utilities/responses'
 import { NextFunction, Request, Response, Router } from 'express'
+import { body, validationResult } from 'express-validator'
+import { ResultSetHeader } from 'mysql2'
 
 const booksRouterApi = Router()
 
@@ -9,9 +12,32 @@ booksRouterApi.get(
   '/',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const books_result: any = await booksServices.getAllBooks()
-      const books: BooksModel = books_result
+      const books: Array<BooksModel> = await booksServices.getAllBooks()
       responses.Success(res, books)
+    } catch (error) {
+      responses.InternalServerError(res, error)
+      next(error)
+    }
+  },
+)
+
+booksRouterApi.post(
+  '/create',
+  upload.fields([]),
+  body('name_book').notEmpty().withMessage('name_book body field required'),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return responses.BadRequest(res, errors.array())
+    }
+
+    const { name_book, description_book } = req.body
+    try {
+      const result_insert_book: ResultSetHeader = await booksServices.create({
+        name_book,
+        description_book: description_book ?? null,
+      })
+      responses.Created(res, result_insert_book)
     } catch (error) {
       responses.InternalServerError(res, error)
       next(error)
