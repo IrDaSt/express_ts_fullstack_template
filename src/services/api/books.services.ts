908bff148b66b6e129149e39604fb973e9e86cfd
Daniel W <48021239+danielwj00@users.noreply.github.com>
2022-01-10 15:23:45 +0800

    mysql promise example

--- src/services/api/books.services.ts
+++ src/services/api/books.services.ts
@@ -1,12 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import mysqlconn from '@utilities/mysql'
+import uuidHelper from '@utilities/uuid'
 
 const getAllBooks = async () => {
-  const result = await mysqlconn.query('select * from books')
+  const result: any = await mysqlconn.query(
+    'select * from books order by created_at desc',
+  )
+  return result
+}
+
+const create = async ({
+  name_book,
+  description_book,
+}: {
+  name_book: string
+  description_book: string
+}) => {
+  const result: any = await mysqlconn.query(
+    `
+  insert into books 
+  (id_book, name_book, description_book)
+  values
+  (?,?,?)
+  `,
+    [uuidHelper.generateUUIDV4(), name_book, description_book],
+  )
   return result
 }
 
 const booksServices = {
   getAllBooks,
+  create,
 }
 
 export default booksServices
