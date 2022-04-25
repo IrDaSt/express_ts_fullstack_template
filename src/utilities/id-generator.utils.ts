f98b559d5467c5496e8ef8de24ca81798faf5fdc
Daniel W <danielwj00@gmail.com>
2022-04-26 03:48:41 +0800

    Update id-generator.utils.ts

--- src/utilities/id-generator.utils.ts
+++ src/utilities/id-generator.utils.ts
@@ -4,8 +4,25 @@ const generateUUIDV4 = (): string => {
   const uid: string = uuid.v4()
   return uid
 }
+
+const nextId = ({
+  id_lama,
+  prefix_length,
+}: {
+  id_lama: string
+  prefix_length: number
+}) => {
+  const prefix = id_lama.substring(0, Number(prefix_length))
+  const angka_str = id_lama.substring(Number(prefix_length), id_lama.length)
+  const angka_plus_str = (Number(angka_str) + 1)
+    .toString()
+    .padStart(angka_str.length, '0')
+  return prefix + angka_plus_str
+}
+
 const idGeneratorUtils = {
   generateUUIDV4,
+  nextId,
 }
 
 export default idGeneratorUtils
