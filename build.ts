a5a039bc9ce8b9935f2aac0cb2f87c92c6f943e3
Daniel W <48021239+danielwj00@users.noreply.github.com>
2022-08-26 02:59:06 +0700

    redis, fix build, update package

--- build.ts
+++ build.ts
@@ -1,5 +1,5 @@
-import childProcess from 'child_process'
-import fse from 'fs-extra'
+import childProcess from "child_process"
+import fse from "fs-extra"
 
 const build = async () => {
   try {
@@ -7,52 +7,52 @@ const build = async () => {
     console.info(
       `${new Date().toISOString()} Removing previous build if exist ...`,
     )
-    await remove('./build/')
+    await remove("./build/")
     console.info(
       `${new Date().toISOString()} Removed previous build if exist\n`,
     )
 
     // Compile scss file
     console.info(`${new Date().toISOString()} Compiling scss file ...`)
-    await exec('npm run build:scss', './')
+    await exec("npm run build:scss", "./")
     console.info(`${new Date().toISOString()} Completed compiling scss file\n`)
 
     // Copy front-end files
     console.info(`${new Date().toISOString()} Copying front-end files ...`)
-    await copy('./src/views', './build/views')
+    await copy("./src/views", "./build/views")
     console.info(`${new Date().toISOString()} Copied front-end files\n`)
 
     // Compile back-end files
     console.info(`${new Date().toISOString()} Compiling back-end files ...`)
 
-    // remove index temp typescript from src
-    await remove(`./src/temp-build/`)
-    // Create temp directory for build
-    await makeDir(`./src/temp-build`)
-    // Create new temp file for build
-    await write(`./src/temp-build/index.temp.ts`, '')
-    // Append module alias import
-    await appendText(
-      `./src/temp-build/index.temp.ts`,
-      `import 'module-alias/register'\n`,
-    )
-    // Append index
-    await appendText(
-      `./src/temp-build/index.temp.ts`,
-      await readFile(`./src/index.ts`),
-    )
+    // // remove index temp typescript from src
+    // await remove(`./src/temp-build/`)
+    // // Create temp directory for build
+    // await makeDir(`./src/temp-build`)
+    // // Create new temp file for build
+    // await write(`./src/temp-build/index.temp.ts`, "")
+    // // Append module alias import
+    // await appendText(
+    //   `./src/temp-build/index.temp.ts`,
+    //   `import 'module-alias/register'\n`,
+    // )
+    // // Append index
+    // await appendText(
+    //   `./src/temp-build/index.temp.ts`,
+    //   await readFile(`./src/index.ts`),
+    // )
 
     // Compile typesript
-    await exec('tsc --build tsconfig.prod.json', './')
+    await exec("tsc --build tsconfig.prod.json", "./")
 
-    // remove index.js from build
-    await remove(`./build/index.js`)
-    // rename temp to index
-    await move(`./build/temp-build/index.temp.js`, `./build/index.js`)
-    // remove temp folder from src
-    await remove(`./src/temp-build/`)
-    // remove temp folder from build
-    await remove(`./build/temp-build/`)
+    // // remove index.js from build
+    // await remove(`./build/index.js`)
+    // // rename temp to index
+    // await move(`./build/temp-build/index.temp.js`, `./build/index.js`)
+    // // remove temp folder from src
+    // await remove(`./src/temp-build/`)
+    // // remove temp folder from build
+    // await remove(`./build/temp-build/`)
 
     console.info(`${new Date().toISOString()} Compiled back-end files`)
   } catch (error) {
@@ -110,7 +110,7 @@ function appendText(dest: string, text: string): Promise<void> {
 
 function readFile(dest: string): Promise<string> {
   return new Promise((res, rej) => {
-    return fse.readFile(dest, 'utf8', (err, data) => {
+    return fse.readFile(dest, "utf8", (err, data) => {
       return !!err ? rej(err) : res(data)
     })
   })
--- src/utilities/typeorm.utils.ts
+++ src/utilities/typeorm.utils.ts
@@ -1,18 +1,25 @@
-import config from '@constants/config'
-import { PostsEntity } from '@models/entities/Posts.entity'
-import { UserEntity } from '@models/entities/User.entity'
-import { DataSource, DataSourceOptions } from 'typeorm'
-import { loggerConsole } from './winston.utils'
+import config from "@constants/config"
+import { PostsEntity } from "@models/entities/Posts.entity"
+import { UserEntity } from "@models/entities/User.entity"
+import { DataSource, DataSourceOptions } from "typeorm"
+import { loggerConsole } from "./winston.utils"
 
 const connection_one: DataSourceOptions = {
-  type: 'mariadb',
-  name: 'connection_one',
+  type: "mariadb",
+  name: "connection_one",
   host: config.database.one.host,
   port: config.database.one.port,
   username: config.database.one.user,
   password: config.database.one.password,
   database: config.database.one.database,
   synchronize: false,
+  cache: {
+    type: "redis",
+    options: {
+      host: "localhost",
+      port: 6379,
+    },
+  },
   entities: [PostsEntity, UserEntity],
 }
 
@@ -39,7 +46,7 @@ class TypeOrmConnection {
         loggerConsole.info(`connected to connection_one`)
       })
       .catch((err: any) => {
-        loggerConsole.error('database connection_one error')
+        loggerConsole.error("database connection_one error")
         // eslint-disable-next-line no-console
         console.error({
           error: {
